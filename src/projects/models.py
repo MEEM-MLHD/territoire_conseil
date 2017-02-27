69d26916f5788a71429716b856a4bcae31ecc01d
ouhouhsami <samuel.goldszmidt@gmail.com>
2017-02-27 14:00:39 +0100

    update form and model

--- src/projects/models.py
+++ src/projects/models.py
@@ -57,14 +57,14 @@ class Manager(models.Model):
         return self.label
 
 
-class Skill(models.Model):
-    label = models.CharField(max_length=255)
+# class Skill(models.Model):
+#     label = models.CharField(max_length=255)
 
-    class Meta:
-        verbose_name = u"Compétence"
+#     class Meta:
+#         verbose_name = u"Compétence"
 
-    def __unicode__(self):
-        return self.label
+#     def __unicode__(self):
+#         return self.label
 
 
 class Trigger(models.Model):
@@ -109,14 +109,10 @@ class Project(models.Model):
     name = models.CharField("Nom du projet", max_length=255)
 
     description = models.TextField("Description du projet", blank=True, help_text="5 lignes maximum")
-
-    # PROJECT_LEADER_TYPE_CHOICES = (
-    #     ('public', 'Public'),
-    #     ('private', u'Privé'),
-    # )
-
-    #project_leader_type = models.CharField("Type de porteur de projet", choices=PROJECT_LEADER_TYPE_CHOICES, max_length=255, null=False, blank=False)
-    #project_leader_name = models.CharField("Nom du porteur de projet", max_length=255, blank=True)
+    begin = models.DateField(u"Date de début de projet", blank=True, null=True)
+    end = models.DateField(u"Date prévisionnelle de fin de projet", blank=True, null=True)
+    image = models.ImageField(upload_to="image", null=True, blank=True)
+    url = models.URLField("Pour en savoir plus (lien URL)", blank=True)
 
     region = models.ForeignKey(Region, verbose_name="Région", blank=True, null=True)
     department = models.ForeignKey(Department, verbose_name="Département", blank=True, null=True)
@@ -124,15 +120,15 @@ class Project(models.Model):
     epci_siren = models.CharField("N° SIREN de l'EPCI", max_length=255, blank=True)
     town_name = models.CharField("Non de la commune", max_length=255, blank=True)
     town_insee = models.CharField("Code INSEE de la commune", max_length=255, blank=True)
-    geom = models.GeometryField(u"Périmètre SIG", blank=True, null=True)
+    land = models.CharField("Non du Pays/PETR", max_length=255, blank=True)
+    geom = models.GeometryField(u"Dessiner le contour du projet", blank=True, null=True)
+    shapefile = models.FileField(u"ou importer un ficher shape", upload_to='shapefile/', blank=True, null=True)
 
     referents = models.ManyToManyField('Referent')
 
     themes = models.ManyToManyField(Theme, verbose_name=u"Thématiques")
     themes_others = models.CharField(u"Autres thématiques", max_length=255, blank=True)
 
-    manager = models.ForeignKey(Manager, verbose_name="Pilote", blank=True, null=True)
-    manager_other = models.CharField(u"Autre pilote, préciser", max_length=255, blank=True)
 
     triggers = models.ManyToManyField(Trigger, verbose_name=u"Eléments déclencheur")
     triggers_others = models.CharField(u"Autres éléments déclencheurs", max_length=255, blank=True)
@@ -142,17 +138,26 @@ class Project(models.Model):
 
     structure_challenges = models.TextField(u"Missions / enjeux pour votre structure (5 lignes max.)", blank=True)
 
-    mobilized_skills = models.ManyToManyField(Skill, verbose_name=u"Compétences mobilisées", related_name="mobilized_skill")
-    mobilized_skills_others = models.CharField(u"Autres compétences mobilisées", max_length=255, blank=True)
-    missing_skills = models.ManyToManyField(Skill, verbose_name=u"Compétences manquantes", related_name="missing_skill")
-    missing_skills_others = models.CharField(u"Autres compétences manquantes", max_length=255, blank=True)
+    # mobilized_skills = models.ManyToManyField(Skill, verbose_name=u"Compétences mobilisées", related_name="mobilized_skill")
+    # mobilized_skills_others = models.CharField(u"Autres compétences mobilisées", max_length=255, blank=True)
+    # missing_skills = models.ManyToManyField(Skill, verbose_name=u"Compétences manquantes", related_name="missing_skill")
+    # missing_skills_others = models.CharField(u"Autres compétences manquantes", max_length=255, blank=True)
 
     schedule = models.ForeignKey(Schedule, verbose_name="Calendrier d'accompagnement")
+
+    STATE_CHOICES = (
+        ('in_progress', 'en cours'),
+        ('completed', u'achevé'),
+    )
+
+    state = models.CharField(u"Etat du projet", choices=STATE_CHOICES, max_length=12, default='in_progress')
     obstables = models.TextField(u"", blank=True, help_text="10 lignes max.")
 
     stakeholders = models.ManyToManyField(StakeHolder, through='StakeHolderType')
 
     comments = models.TextField(u"", blank=True)
+
+    attachment = models.FileField(upload_to="attachment/", null=True, blank=True)
 
     update = models.DateTimeField(auto_now=True)
     creation = models.DateTimeField(auto_now_add=True)
@@ -189,3 +194,4 @@ class StakeHolderType(models.Model):
     stakeholder = models.ForeignKey(StakeHolder, verbose_name="Acteur")
     project = models.ForeignKey(Project)
     detail = models.CharField(max_length=255)
+    manager = models.BooleanField("Pilote", default=None)
